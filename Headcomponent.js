fcecb74cf44d97929e10cb5e04cf287fe83f7a83
ruf32 <rafame099@gmail.com>
2024-01-13 14:45:59 +0100

    fix3

--- Headcomponent.js
+++ Headcomponent.js
@@ -152,7 +152,7 @@ const pages=['java','php','javascript','angular','react','idiomas']
         foot.style.width = '100%';
         foot.style.zIndex = '1000';
         foot.style.height='80px';
-       foot.style.marginLeft='150px'
+       foot.style.marginLeft='10px'
         foot.style.display='grid';
         foot.style.gridTemplateColumns = `repeat(${Object.keys(contac).length}, 1fr)`;
 
